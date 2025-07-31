fe62ec539d35c75d622222ce5d2b4f585dec0b28
Ubuntu <ubuntu@ip-172-31-28-6.us-west-2.compute.internal>
2025-07-31 20:00:23 +0800

    update

--- src/py_az_vm_controller_xethhung12/__main__.py
+++ src/py_az_vm_controller_xethhung12/__main__.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     if os.getenv("DEV") is not None:
         p=os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
         sys.path.insert(0, p)
-    from py_az_vm_controller._cmd import run
+    from py_az_vm_controller_xethhung12._cmd import run
     run.main()
